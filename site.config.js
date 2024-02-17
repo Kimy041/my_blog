d267ce68a3ae645214500597c37c6b6576af984d
kimy041 <miyeong0418@gmail.com>
2024-02-17 17:21:52 +0900

    first

--- site.config.js
+++ site.config.js
@@ -1,29 +1,29 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    name: "MY",
+    image: "/happy.jpeg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    role: "Backend developer",
+    bio: "I develop everything",
+    email: "miyeong0418@gmail.com",
+    //linkedin: "morethanmin",
+    github: "Kimy041",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `kimy-log`,
+      href: "https://github.com/Kimy041/my_blog",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "kimy-log",
+    description: "welcome to kimy-log!",
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
+  link: "https://kimy-log.vercel.app",
   since: 2022, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
