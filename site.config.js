c2715a19ff00062e73d9deff5a9742de0879a2f2
kimy041 <miyeong0418@gmail.com>
2024-02-18 12:51:04 +0900

    -

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "MY",
-    image: "/want.jpeg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/doit.jpeg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "developer",
     bio: "I develop everything",
     email: "miyeong0418@gmail.com",
